cc131e6b838be279ae1ae4610a28234b9d4b5fbf
Andrius Butkevicius <andbtk@telesoftas.com>
2018-05-18 21:20:03 +0300

    Add production cfg

---
+++ config/production.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "auth0": {
+    "domain": "andbtk.eu.auth0.com",
+    "clientID": "vTeWVzC0f5rzDcNdfBODHlL9lqIhn40h",
+    "callbackURL": "https://expressjs-stripe-app.herokuapp.com/callback"
+  }
+};
